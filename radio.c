da66d7bceb86e9b729762a2da41255b8c28ea16c
Palomar <piotr@palomar.pl>
2025-01-19 12:41:42 +0100

    Update radio.c

--- radio.c
+++ radio.c
@@ -813,7 +813,7 @@ void start_radio() {
 	    case DEVICE_HERMES_LITE:
 	    case DEVICE_HERMES_LITE2:
 		have_rx_gain=1;
-		rx_gain_calibration=14;
+		rx_gain_calibration=20;
 		break;
 	    default:
 		have_rx_gain=0;
@@ -826,7 +826,7 @@ void start_radio() {
 	    case NEW_DEVICE_HERMES_LITE:
 	    case NEW_DEVICE_HERMES_LITE2:
 		have_rx_gain=1;
-		rx_gain_calibration=14;
+		rx_gain_calibration=20;
 		break;
 	    default:
 		have_rx_gain=0;
